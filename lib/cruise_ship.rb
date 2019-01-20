5430523ba07ad154a043a020d7d5c74b2f8fd629
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-20 02:53:38 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 passengers = {"Amanda Presley", "Seymour Hoffman", "Alfred Tennyson", "Charlie Chaplin", "Crumpet the Elf"
-suite_a: "Amanda Presley", 
-suite_b: "Seymour Hoffman", 
-suite_c: "Alfred Tennyson", 
-suite_d: "Charlie Chaplin", 
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
 suite_e: "Crumpet the Elf"
 }
 
